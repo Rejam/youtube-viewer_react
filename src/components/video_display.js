fdf1f26b73d3649bbb1e99f0d83beeacf546b7ed
Ryan James <rejames+github@gmail.com>
2018-09-08 11:12:03 +0100

    alignment issues resolved

--- src/components/video_display.js
+++ src/components/video_display.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const VideoDisplay = ({ video }) => {
-  if (!video) { return <div>Loading...</div> }
+  if (!video) {
+    return <div className="container">Loading...</div>;
+  }
 
-  const { videoId } = video.id
-  const url = `https://www.youtube.com/embed/${videoId}`
+  const { videoId } = video.id;
+  const url = `https://www.youtube.com/embed/${videoId}`;
 
-  return(
+  return (
     <div className="col-md-8">
       <div className="embed-responsive embed-responsive-16by9">
-        <iframe 
-          className="embed-responsive-item" 
-          src={ url } />
+        <iframe className="embed-responsive-item" src={url} />
       </div>
-      <div className="details pt-3" style="border: 1px solid #333">
-        <div>{ video.snippet.title }</div>
-        <div> { video.snippet.description }</div>
+      <div className="details pt-3">
+        <div>{video.snippet.title}</div>
+        <div> {video.snippet.description}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default VideoDisplay
+export default VideoDisplay;
--- src/index.js
+++ src/index.js
@@ -1,53 +1,52 @@
-import React from 'react'
-import { render } from 'react-dom'
-import _ from 'lodash'
-import YoutubeSearch from 'youtube-api-search'
-import SearchBar from './components/search_bar'
-import VideoDisplay from './components/video_display'
-import VideoList from './components/video_list'
-import API_KEY from '../config'
+import React from "react";
+import { render } from "react-dom";
+import _ from "lodash";
+import YoutubeSearch from "youtube-api-search";
+import SearchBar from "./components/search_bar";
+import VideoDisplay from "./components/video_display";
+import VideoList from "./components/video_list";
+import API_KEY from "../config";
 
 class App extends React.Component {
   state = {
     videos: [],
     query: "",
-    selectedVideo: null,
-  }
+    selectedVideo: null
+  };
 
-  componentWillMount() { 
-    YoutubeSearch({ key: API_KEY, term: "" }, 
-      videos => this.setState({ videos, selectedVideo: videos[0]}))
+  componentWillMount() {
+    YoutubeSearch({ key: API_KEY, term: "" }, videos =>
+      this.setState({ videos, selectedVideo: videos[0] })
+    );
   }
 
   fetchVideos = () => {
-    YoutubeSearch({
-      key: API_KEY,
-      term: this.state.query,
-    }, videos => this.setState({ videos }))
-  }
+    YoutubeSearch(
+      {
+        key: API_KEY,
+        term: this.state.query
+      },
+      videos => this.setState({ videos })
+    );
+  };
 
-  handleUesrInput = query =>
-    this.setState({ query }, this.fetchVideos)
+  handleUesrInput = query => this.setState({ query }, this.fetchVideos);
 
-  onVideoSelect = selectedVideo =>
-    this.setState({ selectedVideo })
+  onVideoSelect = selectedVideo => this.setState({ selectedVideo });
 
   render() {
-    const handleUesrInput = _.debounce(this.handleUesrInput, 300)
+    const handleUesrInput = _.debounce(this.handleUesrInput, 300);
     return (
       <div className="row">
         <div className="container">
           <h1 className="display-4">YouTube Viewer</h1>
           <SearchBar getUserQuery={handleUesrInput} />
         </div>
-        <VideoDisplay video={ this.state.selectedVideo } />
-        <VideoList 
-          videos={this.state.videos} 
-          getVideo={ this.onVideoSelect }
-        />
+        <VideoDisplay video={this.state.selectedVideo} />
+        <VideoList videos={this.state.videos} getVideo={this.onVideoSelect} />
       </div>
-    )
+    );
   }
 }
 
-render(<App />, document.querySelector('.container'))
+render(<App />, document.querySelector(".container"));
